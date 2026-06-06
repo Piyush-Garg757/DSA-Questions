f7d90f9c4f5e2fc9a348c4e8485ecf5c23dc3627
Piyush <apiyushgarg2006@gmail.com>
2026-06-06 11:03:10 +0530

    Added questions

--- CP31/900/Chemistry.cpp
+++ CP31/900/Chemistry.cpp
@@ -1,4 +1,4 @@
-https://codeforces.com/problemset/problem/1883/B
+https : // codeforces.com/problemset/problem/1883/B
 /*Chemistry
 
 You are given a string s of length n, consisting of lowercase Latin letters, and an integer k.
@@ -73,7 +73,7 @@ In the ninth test case, one occurrence of the characters "t" and "k" can be remo
 
 // My first approach
 #include <bits/stdc++.h>
-using namespace std;
+        using namespace std;
 
 int main()
 {
@@ -116,3 +116,52 @@ int main()
     }
 }
 // TC - O(n)  SC - O(26)
+
+// Slightly better and dimaag lagaya hua
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long t;
+    cin >> t; // Read the number of test cases
+    while (t--)
+    {
+        long long n, k;
+        cin >> n >> k; // Read the length of the string and the number of characters to remove
+        string s;
+        cin >> s; // Read the string
+        // Create a vector to store the frequency of each character ('a' to 'z')
+        vector<int> frequency_of_characters(26, 0);
+        for (int i = 0; i < n; i++)                          // Iterate over the string
+            frequency_of_characters[s[i] - 'a']++;           // Increment the frequency of the current character
+        long long odd_frequency = 0;                         // Variable to count characters with odd frequency
+        for (int i = 0; i < 26; i++)                         // Check each character's frequency
+            odd_frequency += frequency_of_characters[i] % 2; // Count characters with odd frequency
+        // If the number of odd frequency characters is greater than k + 1, it's not possible
+        if (odd_frequency > k + 1)
+            cout << "NO" << endl; // Output NO if not possible
+        else
+            cout << "YES" << endl; // Output YES if possible
+    }
+    return 0;
+}
+
+// Time Complexity (TC): O(n)
+// Space Complexity (SC): O(n)
+
+/*Key Observation
+Suppose there are odd_frequency = x.
+To make a palindrome, we need at most 1 odd frequency.
+Each removed character can reduce the number of odd frequencies by 1.
+So minimum removals needed:
+x - 1
+Therefore:
+x - 1 <= k
+which becomes:
+x <= k + 1
+That's exactly what the code checks:
+if (odd_frequency > k + 1)
+    cout << "NO";
+else
+    cout << "YES";*/
