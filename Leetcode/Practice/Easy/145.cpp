47512ce23b149876b7578f05adb40783d1131776
Piyush <apiyushgarg2006@gmail.com>
2026-06-18 00:44:32 +0530

    Added questions

---
+++ Leetcode/Practice/Easy/145.cpp
@@ -0,0 +1,53 @@
+/*Binary Tree Postorder Traversal
+
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+Explanation:
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,6,7,5,2,9,8,3,1]
+Explanation:
+
+Example 3:
+Input: root = []
+Output: []
+Example 4:
+Input: root = [1]
+Output: [1]*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void postorder(TreeNode *root, vector<int> &ans)
+    {
+        if (root == nullptr)
+            return;
+
+        postorder(root->left, ans);
+        postorder(root->right, ans);
+        ans.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        postorder(root, ans);
+        return ans;
+    }
+};
+// TC - O(n)  SC - O(n)
