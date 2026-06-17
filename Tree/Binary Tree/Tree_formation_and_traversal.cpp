5ac4ba77e29a16214b9a93d0d1abb05e5a7faf36
Piyush <apiyushgarg2006@gmail.com>
2026-06-18 00:17:39 +0530

    Added questions

--- Tree/Binary Tree/Tree_formation_and_traversal.cpp
+++ Tree/Binary Tree/Tree_formation_and_traversal.cpp
@@ -13,11 +13,44 @@ struct node
         left = nullptr;
     }
 };
+void preorder(node *root)
+{
+    if (root == nullptr)
+        return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+    // TC - O(n)  SC - O(n)
+}
+void inorder(node *root)
+{
+    if (root == nullptr)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+    // TC - O(n)  SC - O(n)
+}
+void postorder(node *root)
+{
+    if (root == nullptr)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+    // TC - O(n)  SC - O(n)
+}
+
 int main()
 {
     node *root = new node(2);
     root->left = new node(3);
     root->right = new node(4);
     root->left->right = new node(5);
+    preorder(root);
+    cout << "\n";
+    inorder(root);
+    cout << "\n";
+    postorder(root);
     return 0;
 }
