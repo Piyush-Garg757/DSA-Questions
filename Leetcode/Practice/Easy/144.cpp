fb00e6dac1d168781189eebe634e99ae61667a26
Piyush <apiyushgarg2006@gmail.com>
2026-06-18 16:01:57 +0530

    Added questions

--- Leetcode/Practice/Easy/144.cpp
+++ Leetcode/Practice/Easy/144.cpp
@@ -50,3 +50,41 @@ public:
     }
 };
 // TC - O(n)  SC - O(n)
+
+// Iterative
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        if (root == nullptr)
+            return ans;
+        stack<TreeNode *> st;
+        st.push(root);
+        while (!st.empty())
+        {
+            TreeNode *a = st.top();
+            st.pop();
+            ans.push_back(a->val);
+            if (a->right)
+                st.push(a->right);
+            if (a->left)
+                st.push(a->left);
+        }
+        return ans;
+    }
+};
+// TC - O(n)  SC - O(n)
