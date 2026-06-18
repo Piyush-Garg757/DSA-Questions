bed3d96607b3e6c140c5609aae189bc88e6f615f
Piyush <apiyushgarg2006@gmail.com>
2026-06-18 16:29:04 +0530

    Added questions

---
+++ Tree/Binary Tree/Questions/Max_depth_of_a_binary_tree.cpp
@@ -0,0 +1,78 @@
+/*Maximum Depth of Binary Tree
+
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+// TC - O(n)  SC - O(h)
+
+// BFS Approach
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        queue<TreeNode *> q;
+        int ans = 0;
+        q.push(root);
+        while (!q.empty())
+        {
+            int size = q.size();
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode *a = q.front();
+                q.pop();
+                if (a->left)
+                    q.push(a->left);
+                if (a->right)
+                    q.push(a->right);
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
+// TC - O(n)  SC - O(n)
