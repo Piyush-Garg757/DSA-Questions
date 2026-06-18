10d002d196114495178a04890172f1e348d5573c
Piyush <apiyushgarg2006@gmail.com>
2026-06-18 16:28:30 +0530

    Added questions

--- Leetcode/Practice/Easy/104.cpp
+++ Leetcode/Practice/Easy/104.cpp
@@ -34,3 +34,45 @@ public:
     }
 };
 // TC - O(n)  SC - O(h)
+
+// BFS Approach
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        queue<TreeNode *> q;
+        int ans = 0;
+        q.push(root);
+        while (!q.empty())
+        {
+            int size = q.size();
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode *a = q.front();
+                q.pop();
+                if (a->left)
+                    q.push(a->left);
+                if (a->right)
+                    q.push(a->right);
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
+// TC - O(n)  SC - O(n)
