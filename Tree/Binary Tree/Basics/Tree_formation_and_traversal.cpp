62787ea39cfb3cc965c6605b7b2a6faabdc9527c
Piyush <apiyushgarg2006@gmail.com>
2026-06-18 16:51:51 +0530

    Added questions

--- Tree/Binary Tree/Basics/Tree_formation_and_traversal.cpp
+++ Tree/Binary Tree/Basics/Tree_formation_and_traversal.cpp
@@ -172,6 +172,50 @@ void iterative_postorder_using_1_stack(node *root)
     }
     // TC - O(n)  SC - O(n)
 }
+void all_traversals(node *root)
+{
+    if (root == nullptr)
+        return;
+    vector<int> pre, in, post;
+    stack<pair<node *, int>> st;
+    st.push({root, 1});
+    while (!st.empty())
+    {
+        auto it = st.top();
+        st.pop();
+        if (it.second == 1)
+        {
+            pre.push_back(it.first->data);
+            it.second++;
+            st.push(it);
+            if (it.first->left)
+                st.push({it.first->left, 1});
+        }
+        else if (it.second == 2)
+        {
+            in.push_back(it.first->data);
+            it.second++;
+            st.push(it);
+            if (it.first->right)
+                st.push({it.first->right, 1});
+        }
+        else
+        {
+            post.push_back(it.first->data);
+        }
+    }
+    cout << "Preorder : ";
+    for (int i = 0; i < pre.size(); i++)
+        cout << pre[i] << " ";
+    cout << "\nInorder  : ";
+    for (int i = 0; i < in.size(); i++)
+        cout << in[i] << " ";
+    cout << "\nPostorder: ";
+    for (int i = 0; i < post.size(); i++)
+        cout << post[i] << " ";
+    cout << "\n";
+    // TC - O(n)  SC - O(n)
+}
 int main()
 {
     node *root = new node(2);
@@ -193,5 +237,6 @@ int main()
     cout << "\n";
     iterative_postorder_using_1_stack(root);
     cout << "\n";
+    all_traversals(root);
     return 0;
 }
