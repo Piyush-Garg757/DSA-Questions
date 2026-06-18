89ac3706703c56bde7a570762644017848db7fcc
Piyush <apiyushgarg2006@gmail.com>
2026-06-18 16:16:59 +0530

    Added questions

--- Leetcode/Practice/Easy/145.cpp
+++ Leetcode/Practice/Easy/145.cpp
@@ -51,3 +51,56 @@ public:
     }
 };
 // TC - O(n)  SC - O(n)
+
+// Iterative using 1 stack
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        if (root == nullptr)
+            return ans;
+        stack<TreeNode *> st;
+        while (!st.empty() || root != nullptr)
+        {
+            if (root)
+            {
+                st.push(root);
+                root = root->left;
+            }
+            else
+            {
+                TreeNode *temp = st.top()->right;
+                if (temp == nullptr)
+                {
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while (!st.empty() && (temp == st.top()->right))
+                    {
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else
+                    root = temp;
+            }
+        }
+        return ans;
+    }
+};
+// TC - O(n)  SC - O(n)
