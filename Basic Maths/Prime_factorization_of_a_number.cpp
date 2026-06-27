3404ed6b8062a619dcac605e1035196150b3e2f6
Piyush <apiyushgarg2006@gmail.com>
2026-06-27 22:39:58 +0530

    Added questions

--- Basic Maths/Prime_factorization_of_a_number.cpp
+++ Basic Maths/Prime_factorization_of_a_number.cpp
@@ -34,3 +34,22 @@ public:
     }
 };
 // TC - O(sqrt(n))  SC - O(1)
+
+// Slightly better version
+while (n % 2 == 0)
+{
+    ans.push_back(2);
+    n /= 2;
+}
+for (int i = 3; i * i <= n; i += 2)
+{
+    while (n % i == 0)
+    {
+        ans.push_back(i);
+        n /= i;
+    }
+}
+if (n > 1)
+    ans.push_back(n);
+
+// This is slightly better sinnce we are skipping all even numbers 
