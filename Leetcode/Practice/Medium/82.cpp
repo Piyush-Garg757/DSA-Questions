9162c035242f608daf040076571dbb15e1c0e137
Piyush <apiyushgarg2006@gmail.com>
2026-06-30 22:46:30 +0530

    Added a question

--- Leetcode/Practice/Medium/82.cpp
+++ Leetcode/Practice/Medium/82.cpp
@@ -73,3 +73,70 @@ public:
     }
 };
 // TC - O(n)  SC - O(1)
+
+// My second approach
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        if (!head || !head->next)
+            return head;
+        ListNode *temp = head;
+        ListNode *newhead = nullptr;
+        ListNode *newtemp = nullptr;
+        bool chain = false;
+        while (temp->next)
+        {
+            if (temp->val == temp->next->val)
+            {
+                chain = true;
+            }
+            else
+            {
+                if (!chain)
+                {
+                    if (!newhead)
+                    {
+                        newhead = temp;
+                        newtemp = newhead;
+                    }
+                    else
+                    {
+                        newtemp->next = temp;
+                        newtemp = temp;
+                    }
+                }
+                chain = false;
+            }
+            temp = temp->next;
+        }
+        if (!chain)
+        {
+            if (!newhead)
+            {
+                newhead = temp;
+                newtemp = newhead;
+            }
+            else
+            {
+                newtemp->next = temp;
+                newtemp = temp;
+            }
+        }
+        if (newtemp)
+            newtemp->next = nullptr;
+        return newhead;
+    }
+};
+// TC - O(n)  SC - O(1)
