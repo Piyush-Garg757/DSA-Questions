d6e54acb7587da0563bb4188fcdc81364e934410
Piyush <apiyushgarg2006@gmail.com>
2026-07-02 00:45:18 +0530

    Added a question

--- Striver's A-Z DSA Course/Stack/Basics/Stack_using_array.cpp
+++ Striver's A-Z DSA Course/Stack/Basics/Stack_using_array.cpp
@@ -8,30 +8,26 @@ public:
     {
         top = -1;
     }
-
     void push(int x)
     {
-        arr[++top] = x;
+        if (top != 100000 - 1)
+            arr[++top] = x;
     }
-
     void pop()
     {
         if (top != -1)
             top--;
     }
-
     int peek()
     {
         if (top == -1)
             return -1;
         return arr[top];
     }
-
     bool isEmpty()
     {
         return top == -1;
     }
-
     int size()
     {
         return top + 1;
