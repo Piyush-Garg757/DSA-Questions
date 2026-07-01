2463442ef8abfce808c8dd74c6a75979ee534176
Piyush <apiyushgarg2006@gmail.com>
2026-07-02 00:43:55 +0530

    Added a question

---
+++ Striver's A-Z DSA Course/Stack/Basics/Stack_using_array.cpp
@@ -0,0 +1,39 @@
+class Stack
+{
+    int arr[100000];
+    int top;
+
+public:
+    Stack()
+    {
+        top = -1;
+    }
+
+    void push(int x)
+    {
+        arr[++top] = x;
+    }
+
+    void pop()
+    {
+        if (top != -1)
+            top--;
+    }
+
+    int peek()
+    {
+        if (top == -1)
+            return -1;
+        return arr[top];
+    }
+
+    bool isEmpty()
+    {
+        return top == -1;
+    }
+
+    int size()
+    {
+        return top + 1;
+    }
+};
