341333495e08c1fa5d1c78f4cc1e046a02ac23fa
Piyush <apiyushgarg2006@gmail.com>
2026-07-05 17:47:08 +0530

    Added a question

---
+++ Leetcode/Practice/Medium/143.cpp
@@ -0,0 +1,72 @@
+/*Reorder List
+
+You are given the head of a singly linked-list. The list can be represented as:
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+Example 2:
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]*/
+
+// Simple idea - find middle -> reverse -> join
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *firstMiddle(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast->next && fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *prev = nullptr;
+        ListNode *curr = head;
+        while (curr)
+        {
+            ListNode *nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+    void reorderList(ListNode *head)
+    {
+        ListNode *mid = firstMiddle(head);
+        ListNode *newtemp = reverseList(mid->next);
+        mid->next = nullptr;
+        ListNode *temp = head;
+        while (temp && newtemp)
+        {
+            ListNode *nxt = temp->next;
+            temp->next = newtemp;
+            newtemp = newtemp->next;
+            temp = temp->next;
+            temp->next = nxt;
+            temp = temp->next;
+        }
+    }
+};
+// TC - O(n)  SC - O(1)
