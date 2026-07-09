ed005966f72c4d750a7149bf44e9b59dc3401c93
Piyush <apiyushgarg2006@gmail.com>
2026-07-09 15:53:24 +0530

    Added a question

--- Striver's A-Z DSA Course/Reccursion/N_Queen.cpp
+++ Striver's A-Z DSA Course/Reccursion/N_Queen.cpp
@@ -13,6 +13,7 @@ Example 2:
 Input: n = 1
 Output: [["Q"]]*/
 
+// Intution curr_col lwkw chalo aur har col mein optimally place karte chalo check karte hue ki yaha place kar sakte hai ya nahi aur aise hi aagr n tak pahhuch gaya curr_sol to matlab ki ek sol mila hai
 class Solution
 {
 public:
@@ -64,3 +65,66 @@ public:
     }
 };
 // TC - O(n^2*n!)  SC - O(n^2)
+
+// Slight optimisation in is_safe - rather than checking all previous queens and eliminating positions in thr curr_col whih cant be filled we check which previous queen collide with the current one and if we cant find one we return true
+class Solution
+{
+public:
+    bool is_safe(int row, int col, vector<string> &v, int n)
+    {
+        int r = row, c = col;
+        while (c >= 0)
+        {
+            if (v[r][c] == 'Q')
+                return false;
+            c--;
+        }
+        c = col;
+        while (c >= 0 && r >= 0)
+        {
+            if (v[r][c] == 'Q')
+                return false;
+            c--;
+            r--;
+        }
+        r = row, c = col;
+        while (c >= 0 && r < n)
+        {
+            if (v[r][c] == 'Q')
+                return false;
+            c--;
+            r++;
+        }
+        return true;
+    }
+    void solve(int n, vector<vector<string>> &ans, vector<string> &v,
+               int curr_col)
+    {
+        if (curr_col == n)
+        {
+            ans.push_back(v);
+            return;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (is_safe(i, curr_col, v, n))
+            {
+                v[i][curr_col] = 'Q';
+                solve(n, ans, v, curr_col + 1);
+                v[i][curr_col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n)
+    {
+        vector<vector<string>> ans;
+        vector<string> v(n);
+        string s(n, '.');
+        for (int i = 0; i < n; i++)
+            v[i] = s;
+        int curr_col = 0;
+        solve(n, ans, v, curr_col);
+        return ans;
+    }
+};
+// TC - O(n*n!)  SC - O(n^2)
