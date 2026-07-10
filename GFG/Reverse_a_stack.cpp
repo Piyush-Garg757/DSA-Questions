812f37a33bd1455476b43e49f5be4e95f166ed2d
Piyush <apiyushgarg2006@gmail.com>
2026-07-10 14:23:51 +0530

    Added a question

--- GFG/Reverse_a_stack.cpp
+++ GFG/Reverse_a_stack.cpp
@@ -34,3 +34,31 @@ public:
     }
 };
 // TC - O(n)  SC - O(n)
+
+class Solution
+{
+public:
+    void insertAtBottom(stack<int> &st, int x)
+    {
+        if (st.empty())
+        {
+            st.push(x);
+            return;
+        }
+        int top = st.top();
+        st.pop();
+        insertAtBottom(st, x);
+        st.push(top);
+    }
+
+    void reverseStack(stack<int> &st)
+    {
+        if (st.empty())
+            return;
+        int top = st.top();
+        st.pop();
+        reverseStack(st);
+        insertAtBottom(st, top);
+    }
+};
+// TC - O(n^2)  SC - O(n)
