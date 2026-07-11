753e892a847224a586d0402c606c3a50ef2d806f
Piyush <apiyushgarg2006@gmail.com>
2026-07-11 15:15:11 +0530

    Added questions

--- Make_it_Divisible_by_25.cpp
+++ Make_it_Divisible_by_25.cpp
@@ -1,4 +1,40 @@
+https://codeforces.com/problemset/problem/1593/B
+/*Make it Divisible by 25
 
+It is given a positive integer n. In 1 move, one can select any single digit and remove it (i.e. one selects some position in the number and removes the digit located at this position). The operation cannot be performed if only one digit remains. If the resulting number contains leading zeroes, they are automatically removed.
+E.g. if one removes from the number 32925 the 3-rd digit, the resulting number will be 3225. If one removes from the number 20099050 the first digit, the resulting number will be 99050 (the 2 zeroes going next to the first digit are automatically removed).
+What is the minimum number of steps to get a number such that it is divisible by 25 and positive? It is guaranteed that, for each n occurring in the input, the answer exists. It is guaranteed that the number n has no leading zeros.
+
+Input
+The first line contains one integer t (1≤10^4) — the number of test cases. Then t test cases follow.
+Each test case consists of one line containing one integer n (25≤n≤10^18). It is guaranteed that, for each n occurring in the input, the answer exists. It is guaranteed that the number n has no leading zeros.
+
+Output
+For each test case output on a separate line an integer k (k≥0) — the minimum number of steps to get a number such that it is divisible by 25 and positive.
+
+Example
+
+Input
+5
+100
+71345
+3259
+50555
+2050047
+
+Output
+0
+3
+1
+3
+2
+
+Note
+In the first test case, it is already given a number divisible by 25.
+In the second test case, we can remove the digits 1, 3, and 4 to get the number 75.
+In the third test case, it's enough to remove the last digit to get the number 325.
+In the fourth test case, we can remove the three last digits to get the number 50.
+In the fifth test case, it's enough to remove the digits 4 and 7.*/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -12,9 +48,8 @@ int main()
         long long n;
         cin >> n;
         int steps = 0;
-        int a = 1;
-        bool first_0 = false, second_0 = false, first_5 = false, second_5 = false, second_2 = false, second_7 = false;
-        while (n && a != -1)
+        bool first_0 = false, first_5 = false;
+        while (n)
         {
             int ld = n % 10;
             if (ld == 0)
@@ -22,21 +57,42 @@ int main()
                 if (first_0 == false)
                     first_0 = true;
                 else
-                    second_0 = true;
+                {
+                    if (first_5)
+                        steps++;
+                    break;
+                }
             }
             else if (ld == 5)
             {
                 if (first_5 == false)
+                {
                     first_5 = true;
+                    if (first_0)
+                    {
+                        break;
+                    }
+                }
                 else
-                    second_5 = true;
+                {
+                    steps++;
+                    if (first_0)
+                    {
+                        break;
+                    }
+                }
             }
-            else if (ld == 7)
-                second_7 = true;
-            else if (ld == 2)
-                second_2 = true;
-            if (second_0 == true || (first_5 == true && (second_2 == true || second_7 == true)))
-                a = -1;
+            else if (ld == 7 || ld == 2)
+            {
+                if (first_5)
+                {
+                    if (first_0)
+                        steps++;
+                    break;
+                }
+                else
+                    steps++;
+            }
             else
                 steps++;
             n /= 10;
@@ -44,3 +100,4 @@ int main()
         cout << steps << "\n";
     }
 }
+// TC - O(logn)  SC - O(1)
