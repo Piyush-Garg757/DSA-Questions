5ee5838978b0b5dd915b602da5093388380e1ad7
Piyush <apiyushgarg2006@gmail.com>
2026-07-20 15:14:44 +0530

    Added questions

---
+++ Leetcode/Easy/3280.cpp
@@ -0,0 +1,55 @@
+/*Convert Date to Binary
+
+You are given a string date representing a Gregorian calendar date in the yyyy-mm-dd format.
+date can be written in its binary representation obtained by converting year, month, and day to their binary representations without any leading zeroes and writing them down in year-month-day format.
+Return the binary representation of date.
+
+Example 1:
+Input: date = "2080-02-29"
+Output: "100000100000-10-11101"
+Explanation:
+100000100000, 10, and 11101 are the binary representations of 2080, 02, and 29 respectively.
+
+Example 2:
+Input: date = "1900-01-01"
+Output: "11101101100-1-1"
+Explanation:
+11101101100, 1, and 1 are the binary representations of 1900, 1, and 1 respectively.*/
+
+class Solution
+{
+public:
+    string dec_to_bin(int n)
+    {
+        if (!n)
+            return "0";
+        string s;
+        while (n)
+        {
+            if (n & 1)
+                s.push_back('1');
+            else
+                s.push_back('0');
+            n >>= 1;
+        }
+        reverse(s.begin(), s.end());
+        return s;
+    }
+    string convertDateToBinary(string date)
+    {
+        int n = date.length(), j = 0, i = 0;
+        string ans = "";
+        while (i < n)
+        {
+            while (i < n && date[i] != '-')
+                i++;
+            ans += dec_to_bin(stoi(date.substr(j, i - j)));
+            if (i != n)
+                ans += '-';
+            j = i + 1;
+            i++;
+        }
+        return ans;
+    }
+};
+// TC - O(1)  SC - O(n)
